818e3e9a41db7b08f70158776e59766cfe44f598
Andrii Shybunko <andriishybunko@Andriis-MacBook-Air.local>
2023-03-03 19:37:05 +0200

    Main screen

---
+++ lib/screens/main.screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:the_movie_app/controllers/bottom_navigator_controller.dart';
+
+class Main extends StatelessWidget {
+  Main({Key? key}) : super(key: key);
+  final BottomNavigatorController controller =
+      Get.put(BottomNavigatorController());
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          body: IndexedStack(
+            index: controller.index.value,
+            children: Get.find<BottomNavigatorController>().screens,
+          ),
+          bottomNavigationBar: SizedBox(
+            height: 100,
+            child: BottomNavigationBar(
+              currentIndex: controller.index.value,
+              onTap: (index) =>
+                  Get.find<BottomNavigatorController>().setIndex(index),
+              backgroundColor: const Color(0xFF242A32),
+              selectedItemColor: const Color(0xFF0296E5),
+              unselectedItemColor: const Color(0xFF67686D),
+              selectedFontSize: 12,
+              unselectedFontSize: 10,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: const EdgeInsets.only(bottom: 6),
+                    child: Icon(
+                      CupertinoIcons.home,
+                      color: controller.index.value == 0
+                          ? const Color(0xFF0296E5)
+                          : const Color(0xFF67686D),
+                      size: 30,
+                    ),
+                  ),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: const EdgeInsets.only(bottom: 6),
+                    child: Icon(
+                      CupertinoIcons.search,
+                      color: controller.index.value == 1
+                          ? const Color(0xFF0296E5)
+                          : const Color(0xFF67686D),
+                      size: 30,
+                    ),
+                  ),
+                  label: 'Search',
+                  tooltip: 'Search Movies',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
